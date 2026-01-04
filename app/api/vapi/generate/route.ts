c4f025da798030f846131a1c170ae21bb27db313
TANBIR AHAMED <tanbirahamedg@gmail.com>
2026-01-04 22:26:15 +0530

    added violation check

--- app/api/vapi/generate/route.ts
+++ app/api/vapi/generate/route.ts
@@ -30,7 +30,7 @@ export async function POST(request: Request) {
         : "No specific technologies";
 
     const { text: questions } = await generateText({
-      model: google("gemini-2.5-pro"),
+      model: google("gemini-2.5-flash"),
       prompt: `Prepare questions for a job interview.
         The job role is ${role}.
         The job experience level is ${level}.
